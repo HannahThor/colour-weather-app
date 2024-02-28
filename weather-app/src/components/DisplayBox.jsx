243c7c71c4e24ee17438599fb912625b8515b094
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-02-28 10:33:06 +0000

    undone deleted display box

--- weather-app/src/components/DisplayBox.jsx
+++ weather-app/src/components/DisplayBox.jsx
@@ -1,1 +1,9 @@
-// Weather API
+import React from "react";
+import Weather from "./Weather";
+import InitialDisplay from "./InitialDisplay";
+
+export default function DisplayBox() {
+  const city = "london";
+
+  return <main>{city === null ? <InitialDisplay /> : <Weather />}</main>;
+}
