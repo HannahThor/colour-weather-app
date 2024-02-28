bc4a1d88abc9aab71ddfad602ffccb315d586ae5
Hannah Thorley <hannahthorley2014@gmail.com>
2024-02-28 15:09:01 +0000

    searchCity function

--- weather-app/src/components/Search.jsx
+++ weather-app/src/components/Search.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Search({ location }) {
+  const [searchInput, setSearchInput] = useState("London");
+
   const handleClick = async () => {
-    const { temp_max, temp_min } = await location("london");
+    const { temp_max, temp_min } = await location(searchInput);
     console.log(temp_max, temp_min);
+  };
+
+  const searchCity = (e) => {
+    setSearchInput(e.target.value);
   };
 
   return (
     <>
-      <input type="search"></input>
+      <input type="search" onChange={searchCity} />
       <button onClick={handleClick}>Search</button>
     </>
   );
