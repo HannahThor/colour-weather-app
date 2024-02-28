4e55d0406a9cd8c18d31e5b75c3f2aa0c27103e5
Hannah Thorley <hannahthorley2014@gmail.com>
2024-02-28 14:38:52 +0000

    units

--- weather-app/src/components/Main.jsx
+++ weather-app/src/components/Main.jsx
@@ -8,7 +8,7 @@ const location = async (city) => {
     `
     https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
       import.meta.env.VITE_WEATHER_API_KEY
-    }
+    }&units=metric
     `
   );
   const locationData = await locationResponse.json();
