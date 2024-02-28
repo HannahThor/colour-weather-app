89c0b2906cc224330df634a2e91872adc29e6e4b
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-02-28 10:30:55 +0000

    make sure env file is properly on gitignore and not pushed up

--- weather-app/src/components/DisplayBox.jsx
+++ weather-app/src/components/DisplayBox.jsx
@@ -1,9 +1,1 @@
-import React from "react";
-import Weather from "./Weather";
-import InitialDisplay from "./InitialDisplay";
-
-export default function DisplayBox() {
-  const city = "london";
-
-  return <main>{city === null ? <InitialDisplay /> : <Weather />}</main>;
-}
+// Weather API
