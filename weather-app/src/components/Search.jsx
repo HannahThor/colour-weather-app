6daf265bfaef24ba7f57f6ff3a546456be47be3c
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-02-28 14:43:26 +0000

    cleaned up old code - functions we dont need

--- weather-app/src/components/Search.jsx
+++ weather-app/src/components/Search.jsx
@@ -2,34 +2,8 @@ import React from "react";
 
 export default function Search({ location }) {
   const handleClick = async () => {
-    // const response = await fetch(
-    //   "https://api.weatherapi.com/v1/forecast.json?q=birmingham&days=1",
-    //   {
-    //     headers: {
-    //       key: import.meta.env.WEATHER_API_KEY,
-    //     },
-    //   }
-    // );
-    // const data = await response.json();
-    // console.log(data);
-
-    const { lat, lon } = await location("london");
-    console.log(lat, lon);
-
-    // const response = await fetch(
-    //   "https://api.weatherapi.com/v1/forecast.json?q=birmingham&days=1",
-    //   {
-    //     headers: {
-    //       key: import.meta.env.WEATHER_API_KEY,
-    //     },
-    //   }
-    // );
-    // const response = await fetch(
-    //   `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min`
-    // );
-
-    // const data = await response.json();
-    // console.log(data);
+    const { temp_max, temp_min } = await location("london");
+    console.log(temp_max, temp_min);
   };
 
   return (
