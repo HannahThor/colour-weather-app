b74b9acbd9ab7e374320b510e5859bc23358e9b9
Hannah Thorley <hannahthorley2014@gmail.com>
2024-03-06 14:36:34 +0000

    Stitch the weather

--- weather-app/src/components/Header.jsx
+++ weather-app/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <img className="yarn" src="/images/yarn.png" />
-      <h1 className="main-heading">Weather Palette</h1>
+      <h1 className="main-heading">Stitch the Weather</h1>
     </header>
   );
 }
