2e41845686bfcf4eb07a5d1525e86b31699660fc
Hannah Thorley <hannahthorley2014@gmail.com>
2024-04-25 12:58:39 +0100

    search component styled

--- weather-app/src/components/Search.jsx
+++ weather-app/src/components/Search.jsx
@@ -4,7 +4,9 @@ export default function Search({ location, searchCity, handleClick }) {
   return (
     <div className="form">
       <input type="search" onChange={searchCity} />
-      <button onClick={handleClick}>Search</button>
+      <button className="search-button" onClick={handleClick}>
+        <img className="search-img" src="./images/searchimg.png" />
+      </button>
     </div>
   );
 }
