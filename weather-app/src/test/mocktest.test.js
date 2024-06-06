502b32cdf9f64a719db17db56a6d2a367fa4d6ca
Hannah Thorley <hannahthorley2014@gmail.com>
2024-06-06 10:38:28 +0100

    failing test

---
+++ weather-app/src/test/mocktest.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../components/header/Header.jsx";
+
+test("render correctly", () => {
+  render(<Header />);
+  const heading = screen.getByRole("heading", { level: 1 });
+  expect(heading).toHaveTextContent("Stitch the Weather");
+});
