c6a7ae148aae49548aff1e45d81225fcc1758e79
Hannah Thorley <hannahthorley2014@gmail.com>
2024-06-27 14:26:01 +0100

    aligning input and form

--- weather-app/src/components/InitialDisplay/InitialDisplay.jsx
+++ weather-app/src/components/InitialDisplay/InitialDisplay.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function InitialDisplay() {
   return (
     <section>
-      <p>Search your nearest city and we will return the colour of the day</p>
+      <p className="search-your-city">
+        Search your nearest city and we will return the colour of the day
+      </p>
     </section>
   );
 }
